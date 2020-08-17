6ed47c4b1a1a1352cf454e0e63ec79553defa657
alexander215 <alexander.hoskinson@colorado.edu>
2020-08-16 21:56:08 -0700

    Display coupons in ItemContainer if active

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,12 @@ class App extends Component {
     items: [
       { id: 1, name: 'Banana', weight: 0, price: 10 },
       { id: 2, name: 'Apple', weight: 0, price: 20 },
-      { id: 3, name: 'Orange', weight: 0, price: 30 },
+      { id: 3, name: 'Orange', weight: 0, price: 30 }
+    ],
+    coupons: [
+      {id: 1, name: '30_PERCENT', active: false},
+      {id: 2, name: '100_OFF', active: false},
+      {id: 3, name: 'FREE_SHIPPING', active: false},
     ]
   }
 
@@ -16,7 +21,7 @@ class App extends Component {
       <div className="App">
         <h1>Shopping Cart</h1>
         {/* <ItemContainer fruit={item}/> */}
-        <ItemContainer fruit={this.state.items}/>
+        <ItemContainer fruit={this.state.items} coupons={this.state.coupons} />
       </div>
   );
 }
--- src/components/ItemContainer/ItemContainer.js
+++ src/components/ItemContainer/ItemContainer.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import LineItemContainer from '../LineItemContainer/LineItemContainer';
+import CouponContainer from '../CouponContainer/CouponContainer';
 
 
 const ItemContainer = (props) => {
   const lineItems = props.fruit;
+  const coupons = props.coupons;
+
+  const displayLineItems = lineItems.map( element => (
+    <LineItemContainer name={element.name} price={element.price} weight={element.weight}/>
+  ))
+
+  const displayCoupons = coupons.map( element => {
+    if (element.active) return <CouponContainer name={element.name} active={element.active}/>
+  })
 
   return (
     <div>
       <h2>Items</h2>
-      {lineItems.map( element => (
-          <LineItemContainer name={element.name} price={element.price} weight={element.weight}/>
-      ))
-      }
+      {displayLineItems}
+      {displayCoupons}
     </div>
   )
 }
