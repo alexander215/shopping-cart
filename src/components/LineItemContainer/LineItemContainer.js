e97166aec8952e29a19e8ed5eb74453669914b06
alexander215 <alexander.hoskinson@colorado.edu>
2020-08-16 22:24:04 -0700

    Change item weight to input field

--- src/components/LineItemContainer/LineItemContainer.js
+++ src/components/LineItemContainer/LineItemContainer.js
@@ -3,7 +3,10 @@ import React from 'react'
 const LineItemContainer = (props) => {
   return (
     <div>
-      {props.name} | {props.price} | {props.weight}
+      {props.name} | 
+
+      <input type='number' min='0' value={props.weight} /> kg | 
+      ${props.price}
     </div>
   )
 }
